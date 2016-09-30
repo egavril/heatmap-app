64968f76411e3aa554369c40d4304888f452b23d
egavril <e.gavrilyuk@gmail.com>
2016-09-30 16:15:39 -0700

    reduced results to 30

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ $(function() {
 		  url: 'https://www.strava.com/api/v3/athlete/activities',
 		  dataType: 'jsonp',
 		  data: {
-		  	per_page: '50',
+		  	per_page: '30',
 				access_token: 'f22bb7b09c4feeac2deb43b1bbfeb95e699de968',
 		  },
 		  success: function(res) {
